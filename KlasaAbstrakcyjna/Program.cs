e241c555a6ae7fddac7f2d4601b10a3547fb7269
siudekk <siudekk@PLNB1003>
2018-09-26 14:33:44 +0200

    Dodaj pliki projektów.

---
+++ KlasaAbstrakcyjna/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace KlasaAbstrakcyjna
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
